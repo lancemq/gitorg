b46ef9a71b2f611073565002fa10fb7eac928aed
马奇 <maqi@LMs-Mac-mini-M4.local>
2026-06-06 11:04:05 +0800

    move AdSense to <head> (beforeInteractive)

--- app/layout.tsx
+++ app/layout.tsx
@@ -99,7 +99,7 @@ export default function RootLayout({
         <Script
           id="adsense"
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7712476875404468"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
           crossOrigin="anonymous"
         />
         {children}
